ab6a1aef0c9eb22f42e3931d7338ff70413e128a
Shivam <shivam@gehnay.com>
2026-09-13 14:04:11 +0530

    Await connectDB in Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 // Vercel Serverless Function Entry Point
 process.env.VERCEL = '1';
+const connectDB = require('../backend/config/db');
 const app = require('../backend/server');
 
-module.exports = app;
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
